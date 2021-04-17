dee4d2c5364e660e2ade81fe684993e78d688551
Jasper LaFortune <jasperlafortune@gmail.com>
2021-04-17 16:34:30 -0600

    add initial App load test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,1 +1,10 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import "../../setupTests"
+import App from "./App";
 
+it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  

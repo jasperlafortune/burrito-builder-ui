b1fa620bd64791939ed1b03a04df6850373712c9
Jasper LaFortune <jasperlafortune@gmail.com>
2021-04-18 16:12:33 -0600

    add App component tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import {render, fireEvent, cleanup} from '@testing-library/react';
+// import React from "react";
 import { shallow, mount } from "enzyme";
 import "../../setupTests"
 import App from "./App";
 
-it("renders without crashing", () => {
-    shallow(<App />);
-  });
+describe("Test App Component", () => {
+    afterEach(cleanup);
+
+    it("has title Burrito Builder", () => {
+        const { getByText } = render(<App />);
+        expect(getByText(/Burrito/i).textContent).toBe("Burrito Builder")
+    });
+
+    it("loads historic orders", () => {
+        const wrapper = mount(<App />);
+        const orders = wrapper.instance().state.orders;
+        expect(orders.length).toBeDefined();
+    })
+});
+
+
 
   
